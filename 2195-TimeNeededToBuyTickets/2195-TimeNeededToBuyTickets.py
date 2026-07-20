3f8b67d0bc09dc5dfc479448ea7f56ac43df85fa
sanketh-shetty15 <sankethshettykigga15@gmail.com>
2026-07-20 18:37:31 +0530

    Create 2195-TimeNeededToBuyTickets

---
+++ 2195-TimeNeededToBuyTickets/2195-TimeNeededToBuyTickets.py
@@ -0,0 +1,11 @@
+# Last updated: 20/07/2026, 18:37:30
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        t=0
+        for i in range(len(tickets)):
+            if i<=k:
+                t=t+min(tickets[i],tickets[k])
+            else:
+                t+=min(tickets[i],tickets[k]-1)
+        return t
+        
