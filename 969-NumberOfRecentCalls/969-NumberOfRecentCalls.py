b32028cabd91b87b408af99eb15dbe7727fa9c3e
sanketh-shetty15 <sankethshettykigga15@gmail.com>
2026-07-20 18:38:06 +0530

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.py
@@ -0,0 +1,13 @@
+# Last updated: 20/07/2026, 18:38:05
+from collections import deque
+
+class RecentCounter:
+
+    def __init__(self):
+        self.q = deque()
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+        while self.q[0] < t - 3000:
+            self.q.popleft()
+        return len(self.q)
