736473497725254d9eaa68e2de4f82014c7b9cc3
sanketh-shetty15 <sankethshettykigga15@gmail.com>
2026-07-21 23:06:00 +0530

    File updated at 21/07/2026, 23:06:00

--- 1748-SumofUniqueElements/1748-SumofUniqueElements.py
+++ 1748-SumofUniqueElements/1748-SumofUniqueElements.py
@@ -1,10 +1,13 @@
-# Last updated: 21/07/2026, 20:15:29
+# Last updated: 21/07/2026, 23:06:00
 1class Solution:
-2    def sumOfUnique(self, nums: List[int]) -> int:
-3        total=0
-4        for i in nums:
-5            if nums.count(i)==1:
-6                total=total+i
-7        return total
-8
-9        
+2    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+3        intervals.sort(key=lambda x:x[1])
+4        count=0
+5        end=intervals[0][1]
+6        for i in range(1,len(intervals)):
+7            if not intervals[i][0]>=end:
+8                count+=1
+9            else:
+10                end=intervals[i][1]
+11        return count
+12        
