326053fcfce7c5504d8ab1ab75ac11755f815f59
sanketh-shetty15 <sankethshettykigga15@gmail.com>
2026-07-22 22:59:41 +0530

    File updated at 22/07/2026, 22:59:41

--- 56-MergeIntervals/56-MergeIntervals.py
+++ 56-MergeIntervals/56-MergeIntervals.py
@@ -1,11 +1,18 @@
-# Last updated: 20/07/2026, 18:40:06
-class Solution:
-    def merge(self, intervals):
-        intervals.sort()
-        res = [intervals[0]]
-        for s, e in intervals[1:]:
-            if s <= res[-1][1]:
-                res[-1][1] = max(res[-1][1], e)
-            else:
-                res.append([s, e])
-        return res
+# Last updated: 22/07/2026, 22:59:41
+1class Solution:
+2    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+3        stack = []
+4        intervals.sort()
+5
+6        if len(intervals) > 1:
+7            stack.append(intervals[0])
+8
+9            for i in range(1, len(intervals)):
+10                if stack[-1][1] >= intervals[i][0]:
+11                    stack[-1][1] = max(stack[-1][1], intervals[i][1])
+12                else:
+13                    stack.append(intervals[i])
+14        else:
+15            stack = intervals
+16
+17        return stack
