a3d2acba2da9c026f77e5df83f180646cf70ebec
sanketh-shetty15 <sankethshettykigga15@gmail.com>
2026-08-06 20:22:11 +0530

    File updated at 06/08/2026, 20:22:11

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,36 +1,23 @@
-# Last updated: 20/07/2026, 20:41:47
-1# class Solution:
-2#     def lengthOfLongestSubstring(self, s: str) -> int:
-3#         stack=[]
-4#         total=0
-5#         max_sum=0
-6#         if s=="":
-7#             return 0
-8#         elif s==" ":
-9#             return 1
-10#         else:
-11#             for i in range(len(s)):
-12#                 if s[i] not in stack:
-13#                     stack.append(s[i])
-14#                 else:
-15#                     total=len(stack)
-16#                     max_sum=max(max_sum,total)
-17#                     stack.clear()
-18#                     stack.append(s[i])
-19#             return max_sum
-20        # b=set(s)
-21
-22class Solution:
-23    def lengthOfLongestSubstring(self, s: str) -> int:
-24        stack = []
-25        max_len = 0
-26
-27        for ch in s:
-28            while ch in stack:
-29                stack.pop(0)      
-30
-31            stack.append(ch)      
-32            max_len = max(max_len, len(stack))
-33
-34        return max_len
-35        
+# Last updated: 06/08/2026, 20:22:11
+1class Solution:
+2    def threeSum(self,nums:List[int])->List[List[int]]:
+3        nums.sort()
+4        r=[]
+5        for i in range(len(nums)):
+6            if i>0 and nums[i]==nums[i-1]:
+7                continue
+8            l=i+1
+9            h=len(nums)-1
+10            while l<h:
+11                s=nums[i]+nums[l]+nums[h]
+12                if s==0:
+13                    r.append([nums[i],nums[l],nums[h]])
+14                    l+=1
+15                    h-=1
+16                    while l<h and nums[l]==nums[l-1]:
+17                        l+=1
+18                elif s<0:
+19                    l+=1
+20                else:
+21                    h-=1
+22        return r
